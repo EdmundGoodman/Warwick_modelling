5989dff62e3e5a0f246fd1cc813dfb2b1e87c688
EdmundGoodman <egoodman3141@gmail.com>
2021-09-17 17:40:32 +0100

    Added mechanism for user input to model parameters

--- transcrypt_files/__target__/org.transcrypt.__runtime__.js
+++ transcrypt_files/__target__/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2021-09-17 17:19:55
+// Transcrypt'ed from Python, 2021-09-17 17:36:28
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
